b24c9f04f3b8947cf1d5e95ef8ba033e8873c3d2
Vishwas R <vishwas.r@fenopix.com>
2022-01-25 19:53:09 +0530

    Added Splash-Screen

---
+++ app/src/main/java/com/csu/web2app/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.csu.web2app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
